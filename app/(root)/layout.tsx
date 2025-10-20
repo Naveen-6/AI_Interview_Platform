e3f89d325e7961949b3bf06f0c962d756d4b859c
Naveen <naveensai.alapati6@gmail.com>
2025-10-20 15:31:31 -0500

    feat: add main app and route layouts

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,32 @@
+import { isAuthenticated } from '@/lib/actions/auth.action';
+import Image from 'next/image';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import React, { ReactNode } from 'react'
+
+const RootLayout = async ({ children }: { children: ReactNode}) => {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) redirect('/sign-in');
+  
+  return (
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className='flex items-center gap-2'>
+          <Image
+  src="/logo.svg"
+  alt="Logo"
+  width={100}
+  height={100}
+  style={{ width: "100px", height: "auto" }} 
+/>
+
+          <h2 className='text-primary-100'>InterviewPrep.AI</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  )
+}
+
+export default RootLayout;
